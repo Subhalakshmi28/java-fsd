07577775d20fae9cd93627eeaa980fccf58ddf95
subhalakshmi <subhalakshmim2001@gmail.com>
2022-07-27 22:46:31 +0530

    java-fsd

---
+++ phase1/package/accessspecifiers1.java
@@ -0,0 +1,24 @@
+package pratice;
+class defAccessSpecifier
+{ 
+  void display() 
+     { 
+         System.out.println("You are using defalut access specifier"); 
+     } 
+} 
+
+
+
+public class accessspecifiers1 {
+
+	public static void main(String[] args) {
+		//default
+		System.out.println("Dafault Access Specifier");
+		defAccessSpecifier obj = new defAccessSpecifier(); 		  
+        obj.display(); 
+
+	}
+}
+
+
+
---
+++ phase1/package/accessspecifiers2.java
@@ -0,0 +1,21 @@
+package pratice;
+class priaccessspecifier 
+{ 
+   private void display() 
+    { 
+        System.out.println("You are using private access specifier"); 
+    } 
+} 
+
+
+
+public class accessspecifiers2 {
+	public static void main(String[] args) {
+		System.out.println("Private Access Specifier");
+		priaccessspecifier  obj = new priaccessspecifier(); 
+        
+	}
+}
+
+
+
