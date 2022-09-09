534ac63a6ee7fc9faa3c3908bda7a8373a1a885c
subhalakshmi <subhalakshmim2001@gmail.com>
2022-09-09 13:27:09 +0530

    java-fsd

---
+++ phase1/Handling/src/main/java/com/UserAuthentication.java
@@ -0,0 +1,16 @@
+package com;
+
+public class UserAuthentication {
+	public String getUserName() {
+		return "subha";
+	}
+	public String getEmail() {
+		return "subha@gmail.com";
+	}
+	public int getPhnNum() {
+		return 123456789;
+	}
+	public String getPassword() {
+		return "subha@123";
+	}
+}
---
+++ phase1/Handling/src/test/java/com/UserAuthenticationTest.java
@@ -0,0 +1,36 @@
+package com;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class UserAuthenticationTest {
+	private UserAuthentication userA;
+	@BeforeEach
+	public void setup() {
+		userA=new UserAuthentication();
+		System.out.println("Object initialized");
+	}
+	@Test
+	public void verifyUserName() {
+		assertEquals(userA.getUserName(), "subha");
+	}
+	@Test
+	public void verifyEmail() {
+		assertEquals(userA.getEmail(), "subha@gmail.com");
+	}
+	@Test
+	public void verifyPhnNum() {
+		assertEquals(userA.getPhnNum(), 123456789);
+	}
+	@Test
+	public void verifyPassword() {
+		assertEquals(userA.getPassword(), "subha@123");
+	}
+	@AfterEach
+	public void tearDown() {
+		userA=null;
+		System.out.println("Object destroyed");
+	}
+}
